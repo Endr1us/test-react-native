ba7963b4ee79dee6aea8c2d4264584ebd67ceacd
Endr1us <54222451+Endr1us@users.noreply.github.com>
2020-03-09 00:59:51 +0200

    Update ModalWind.js

--- src/components/ModalWind.js
+++ src/components/ModalWind.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { Modal, Text, TouchableHighlight, View, Alert } from "react-native";
+import {
+  Modal,
+  Text,
+  TouchableHighlight,
+  View,
+  Alert,
+  StyleSheet
+} from "react-native";
 import Apiloader from "./Apiloader";
+import styled from "styled-components/native";
 
 export default class ModalWind extends React.Component {
   state = {
@@ -13,7 +21,7 @@ export default class ModalWind extends React.Component {
 
   render() {
     return (
-      <View style={{ marginTop: 1 }}>
+      <ContainerView>
         <Modal
           animationType="slide"
           transparent={false}
@@ -22,9 +30,10 @@ export default class ModalWind extends React.Component {
             Alert.alert("Modal has been closed.");
           }}
         >
-          <View style={{ marginTop: 10 }}>
+          <View style={styles.modalWind}>
             <View>
-              <Text style={{ marginTop: 10 }}>
+              <Text style={styles.topText}>Be happy!!!</Text>
+              <Text style={styles.topWindUser}>
                 Вы действительно хотите пригласить {this.props.title} на
                 свидание?
               </Text>
@@ -33,15 +42,16 @@ export default class ModalWind extends React.Component {
                 onPress={() => {
                   this.setModalVisible(!this.state.modalVisible);
                 }}
+                style={{ marginBottom: 30 }}
               >
-                <Text style={{ marginTop: 30 }}>Да</Text>
+                <Text style={styles.windUser}>Да</Text>
               </TouchableHighlight>
               <TouchableHighlight
                 onPress={() => {
                   this.setModalVisible(false);
                 }}
               >
-                <Text style={{ marginTop: 30 }}>Нет</Text>
+                <Text style={styles.windUser}>Нет</Text>
               </TouchableHighlight>
             </View>
           </View>
@@ -54,7 +64,50 @@ export default class ModalWind extends React.Component {
         >
           <Text>{this.props.title}</Text>
         </TouchableHighlight>
-      </View>
+      </ContainerView>
     );
   }
 }
+
+const ContainerView = styled.View`
+  color: ${item => (item.status == "active" ? "black" : "grey")};
+  margin-top: 10px;
+`;
+
+const styles = StyleSheet.create({
+  modalWind: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: "#8abbe3",
+    textAlign: "center"
+  },
+  topWindUser: {
+    borderColor: "#0b1dde",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 10,
+    marginVertical: 30,
+    paddingVertical: 30,
+    backgroundColor: "#d6a7a7",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    fontSize: 20
+  },
+  windUser: {
+    borderColor: "#000fb0",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 15,
+    paddingVertical: 30,
+    backgroundColor: "#bf80bb",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    fontSize: 16
+  },
+  topText: {
+    textAlign: "center",
+    fontSize: 28
+  }
+});
